ccbd17760a2f2c67fab4ef530a4fe50d1e5a146b
Constant62 <liebart.constant@gmail.com>
2019-07-08 17:33:41 +0200

    poney

--- app/app.php
+++ app/app.php
@@ -1,4 +1,21 @@
 <?php
 /**
- * 1. 
+ * 1. Démarrage de la session
  */
+
+ /**
+  * 2. Intégration de la configuration
+  */
+
+  /**
+   * 3. Définition de l'environnement
+   */
+
+   /**
+    * 4. Comportement des erreurs
+    */
+
+    /**
+     * 5. Connection aux bases de données
+     */
+    
